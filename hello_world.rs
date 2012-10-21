6cffbc8b62f26ae42219aa1c3607494d6c89192f
Kirill Panshin <kipanshi@gmail.com>
2012-10-21 23:50:02 +0700

    Add borrowed pointers example

--- hello_world.rs
+++ hello_world.rs
@@ -1,5 +1,6 @@
 use std;
 use io::println;
+use float::sqrt;
 
 fn factorial(x: uint) -> uint {
     if (x <= 1u) {
@@ -14,7 +15,7 @@ fn print_current_time() {
     println(fmt!("Current timestamp: %?", current_time));
 }
 
-fn use_managed_box() {
+fn managed_box() {
     let x: @int = @10; // New box, immutable int pointer
     let y = x; // Copy of a pinter to the same box
 
@@ -24,6 +25,7 @@ fn use_managed_box() {
     fn print_box(x: @int, y: @int) {
         println("");
         println("Managed box usage:");
+        println("");
         println(fmt!("    Variable x with address %? has value %?", x, *x));
         println(fmt!("    Variable y with address %? has value %?", y, *y));
     }
@@ -31,7 +33,7 @@ fn use_managed_box() {
     print_box(x, y);
 }
 
-fn use_owned_box() {
+fn owned_box() {
     /* Owned box has unique memory slot */
     let x = ~10;
     /* Doing this:
@@ -52,8 +54,43 @@ fn use_owned_box() {
     */
 }
 
+fn borrowed_pointers() {
+    struct Point {
+        name: ~str,
+        x: float, y: float
+    };
+
+    let on_the_stack : Point  =  Point {name: ~"on_the_stack", x: 3.0, y: 4.0};
+    let shared_box   : @Point = @Point {name: ~"shared_box", x: 5.0, y: 1.0};
+    let unique_box   : ~Point = ~Point {name: ~"unique_box", x: 7.0, y: 9.0};
+
+    /* We want to compute distance between two points
+    no matter how these points are stored */
+    fn compute_distance(p1: &Point, p2: &Point) -> float {
+        let x_d = p1.x - p2.x;
+        let y_d = p1.y - p2.y;
+        sqrt(x_d * x_d + y_d * y_d)
+    }
+
+    fn print_distance(p1: &Point, p2: &Point) {
+        let distance = compute_distance(p1, p2);
+        println("");
+        println(
+            fmt!("    Distance from \n    %?\n    to\n    %?\n    equals %f",
+                 p1, p2, distance)
+        );
+    }
+
+    println("");
+    println("Borrowed pointers example:");
+    print_distance(&on_the_stack, shared_box);
+    print_distance(shared_box, unique_box);
+    
+}
+
 fn main() {
     println(fmt!("Factorial of 5 is %?", factorial(5)));
-    use_managed_box();
-    use_owned_box();
+    managed_box();
+    owned_box();
+    borrowed_pointers();
 }
